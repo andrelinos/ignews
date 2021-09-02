04619232f61014226993e07b81a168e4ab3687b2
Andrelino Silva <andrelinocom@gmail.com>
2021-09-02 08:10:25 -0300

    Add preview (teste)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,8 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,900&display=swap" rel="stylesheet" />
 
+          <script async defer src="https://static.cdn.prismic.io/prismic.js?new=true&repo=and-ignews" />
+
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
